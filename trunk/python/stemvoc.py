c98ab5d7028d0a0e75943dd20fc2ac015e2a7110
barliant@gmail.com <barliant@gmail.com@be4f7f82-f96f-c3c5-2885-5b211381c0ca>
2012-06-01 10:43:08 +0000

    Stemming using VK pattern.

--- trunk/python/stemvoc.py
+++ trunk/python/stemvoc.py
@@ -14,22 +14,68 @@
 # ber-, ter-, me-, di-, ke-, ku-, kau-, pe-, per-, se-
 
 def main():
+    kata = raw_input("Masukkan sebuah kata: ")
+    kata = kata.lower()
+    kata = kata.strip()
+    pola = vocalize(kata)
+    print pola
+    
+
+def vocalize(word):
+    '''
+    >>> vocalize('apa')
+    VKV
+    >>> vocalize('sungguh')
+    KVKKVK
+    '''
+
     vocal = ['a', 'i', 'u', 'e', 'o']
     consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
         'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 
-    kata = raw_input("Masukkan sebuah kata: ")
-    kata = kata.lower()
-    kata = kata.strip()
+    NORMAL, PREDIFTONG, PREMIXED = range(3)
 
-    for huruf in kata:
+    pola = ''
+
+    state = NORMAL
+
+    for huruf in word:
         if huruf in vocal:
-            print 'V',
-        elif huruf in consonant:
-            print 'K',
-        else:
-            print '-',
+            if huruf in ['a', 'o']:
+                prev_letter = huruf
+                continue
+            if huruf in ['u'] and prev_letter == 'a':
+                pola = pola + 'V'
+                continue
+            if huruf in ['i'] and prev_letter in ['a', 'o']:
+                pola = pola + 'V'
+                continue
+            if prev_letter in consonant:
+                pola = pola + 'KV'
+                continue
+            pola = pola+'V'
+            continue
+        if huruf in consonant:
+            if huruf in ['k', 'n', 's']:
+                prev_letter = huruf
+                continue
+            if huruf == 'y' and prev_letter in ['n', 's']:
+                pola = pola + 'K'
+                continue
+            if huruf == 'g' and prev_letter == 'n':
+                pola = pola + 'K'
+                continue
+            if huruf == 'h' and prev_letter == 'k':
+                pola = pola + 'K'
+                continue
+            if prev_letter in vocal:
+                pola = pola + 'VK'
+                continue
+            pola = pola+'K'
+            continue
+        pola = pola+'-'
 
+    return pola
 
 
 if __name__ == '__main__':
