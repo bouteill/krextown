b9e23b422175cdfc2f3ae2063f21633e2d6a15f4
barliant <barliant@be4f7f82-f96f-c3c5-2885-5b211381c0ca>
2013-02-02 03:53:40 +0000

    Utak-atik graftempo. Weekly commit.

--- python/graftempo.py
+++ python/graftempo.py
@@ -2,24 +2,30 @@
 import os
 import nltk.data
 
-print nltk.data.path
-
 from nltk.corpus import PlaintextCorpusReader
 
-corpus_dir = '../corpus-local/tempo-txt'
-corpus_root = os.getcwd() + '/' + corpus_dir
-wordlists = PlaintextCorpusReader(corpus_root, '.*\.txt')
 
-len(wordlists.fileids())
+def main():
+  print nltk.data.path
 
-f1 = wordlists.fileids()[0]
-f2 = wordlists.fileids()[1]
-f3 = wordlists.fileids()[2]
+  corpus_dir = '../corpus-local/tempo-txt'
+  corpus_root = os.getcwd() + '/' + corpus_dir
+  wordlists = PlaintextCorpusReader(corpus_root, '.*\.txt')
 
-wordlists.raw(fileids=[f1,f2,f3])
+  len(wordlists.fileids())
 
-for sent in wordlists.sents(fileids=[f1]):
-  for word in sent:
-    print word,
-  print
+  f1 = wordlists.fileids()[0]
+  f2 = wordlists.fileids()[1]
+  f3 = wordlists.fileids()[2]
+
+  wordlists.raw(fileids=[f1,f2,f3])
+
+  for sent in wordlists.sents(fileids=[f1]):
+    for word in sent:
+      print word,
+    print
+
+
+if __name__ == '__main__':
+  main()
 
