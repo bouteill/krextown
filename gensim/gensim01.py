e95b2d174136c5369ada350ae024b48ca6b9322b
barliant <barliant@gmail.com>
2014-10-30 10:14:05 +0000

    Gensim, LogBook, and Logistic Map.

--- gensim/gensim01.py
+++ gensim/gensim01.py
@@ -1,11 +1,13 @@
 from gensim import corpora, models, similarities
+import logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
 class Gensim01():
     def __init__(self):
         self.corpus = [[(0, 1.0), (1, 1.0), (2, 1.0)],
-            [(2, 1.0), (3, 1.0), (4, 1.0), (5, 1.0), (6, 1.0), (8,
-              1.0)]
+            [(2, 1.0), (3, 1.0), (4, 1.0), (5, 1.0), (6, 1.0), (8, 1.0)]
             [(1, 1.0), (3, 1.0), (4, 1.0), (7, 1.0)],
             [(0, 1.0), (4, 2.0), (7, 1.0)],
             [(3, 1.0), (5, 1.0), (6, 1.0)],
